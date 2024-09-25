6e9ae41738d05e5389ae001fcbdc3fb8bf8913f6
Elihú <a23elivalyen@inspedralbes.cat>
2024-09-25 11:34:59 +0200

    act

--- back/index.php
+++ back/index.php
@@ -7,29 +7,42 @@ if (isset($_GET['reiniciar'])) {
 }
 
 $datos = file_get_contents("data.json");
-$String = json_decode($datos, true);
-shuffle($String);
-if (!isset($_SESSION['preguntas_aleatorias'])) {
-    shuffle($String); // Barajar las preguntas
-    $_SESSION['preguntas_aleatorias'] = array_slice($String, 0, 10); // Guardar 10 preguntas aleatorias
-    $_SESSION['indice'] = 0; // Inicializar el índice
-}
+$preguntas_array = json_decode($datos, true);
 
+if (!isset($_SESSION['preguntas_aleatorias'])) {
+    shuffle($preguntas_array); 
+    $_SESSION['preguntas_aleatorias'] = array_slice($preguntas_array, 0, 10); // Guardar 10 preguntas aleatorias
+    $_SESSION['indice'] = 0; 
+}
 
 $indice = isset($_GET['indice']) ? intval($_GET['indice']) : $_SESSION['indice'];
 
 if ($indice < 0 || $indice >= count($_SESSION['preguntas_aleatorias'])) {
-    $indice = 0; // Volver a la primera pregunta si el índice es inválido
+    $indice = 0; 
 }
 
-$pregunta = $_SESSION['preguntas_aleatorias'][$indice];
+$pregunta_actual = $_SESSION['preguntas_aleatorias'][$indice];
 
-echo "<strong>" . $pregunta['pregunta'] . "</strong><br>";
-echo '<img src="img/' . $pregunta['imatge'] . '" style="width:200px;"><br><br>';
+if (isset($_POST['respuesta'])) {
+    $respuesta_usuario = intval($_POST['respuesta']);
+    $respuesta_correcta = $pregunta_actual['resposta_correcta'];
 
-foreach ($pregunta['respostes'] as $respuesta) {
-    echo '<button>' . $respuesta . '</button> ';
+    if ($respuesta_usuario == $respuesta_correcta) {
+        echo "<p style='color: green;'>¡Correcto!</p>";
+    } else {
+        echo "<p style='color: red;'>Incorrecto</p>";
+    }
 }
+
+echo "<strong>" . $pregunta_actual['pregunta'] . $indice."</strong><br>";
+echo '<img src="img/' . $pregunta_actual['imatge'] . '" style="width:200px;"><br><br>';
+
+echo '<form method="POST">';
+foreach ($pregunta_actual['respostes'] as $index => $respuesta) {
+    echo '<button name="respuesta" value="' . $index . '">' . $respuesta . '</button> ';
+}
+echo '</form>';
+
 echo '<br><br>';
 
 if ($indice > 0) {
@@ -38,9 +51,9 @@ if ($indice > 0) {
 if ($indice < count($_SESSION['preguntas_aleatorias']) - 1) {
     echo '<button><a href="?indice=' . ($indice + 1) . '">Siguiente pregunta</a></button> ';
 }
+
 echo '<br><br>';
 echo '<button><a href="?reiniciar=true">Reiniciar preguntas</a></button>';
 
-//guardar indice
 $_SESSION['indice'] = $indice;
 ?>
