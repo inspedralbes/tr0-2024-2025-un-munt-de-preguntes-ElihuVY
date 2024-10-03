2d179c873b5be8a93f1bfe1484cb04961b775f52
Elihú <a23elivalyen@inspedralbes.cat>
2024-10-03 22:36:26 +0200

    dadadda

--- back/finalitza.php
+++ back/finalitza.php
@@ -2,7 +2,7 @@
 session_start();
 header('Content-Type: application/json');
 
-
+$respuestas_correctas = $_SESSION['respuestas_correctas'];
 $respuestas_usuario = json_decode(file_get_contents('php://input'), true);
 
 if (!isset($respuestas_usuario['respuestas']) || !is_array($respuestas_usuario['respuestas'])) {
@@ -12,12 +12,12 @@ if (!isset($respuestas_usuario['respuestas']) || !is_array($respuestas_usuario['
     exit;
 }
 
-$respuestas_usuario[] = $respuestas_usuario['respuestas'];
+$respuestas_usuario = $respuestas_usuario['respuestas'];
 $respuestas_correctas_usuario = 0;
 $total_respuestas = count($respuestas_usuario);
 
 foreach ($respuestas_usuario as $pregunta_id => $respuesta_usuario) {
-    if (isset($respuestas_correctas[$pregunta_id]) && $respuesta_usuario ==  $_SESSION['respuestas_correctas'][$pregunta_id]) {
+    if (isset($respuestas_correctas[$pregunta_id]) && $respuesta_usuario == $respuestas_correctas[$pregunta_id]) {
         $respuestas_correctas_usuario++;
     }
 }
